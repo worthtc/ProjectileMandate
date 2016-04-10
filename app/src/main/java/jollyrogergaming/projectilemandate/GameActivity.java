30b7465d9fd470a33889cba4155f7840486c2bf8
Kelton Hislop <khislop@mines.edu>
2016-04-09 21:58:45 -0600

    Got user touch position

--- app/src/main/java/jollyrogergaming/projectilemandate/GameActivity.java
+++ app/src/main/java/jollyrogergaming/projectilemandate/GameActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,6 +31,10 @@ public class GameActivity extends AppCompatActivity {
     public static final String EXTRA_IS_GAME_HARD = "jollyrogergaming.projectilemandate.is_game_easy";
     public static final String KEY_COLOR_SCHEMA = "color_scheme";
     public static final String KEY_IS_GAME_HARD = "is_game_easy";
+    private static final String TAG = "GameActivity";
+
+    GameView mGameView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +47,18 @@ public class GameActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        mTestTextOne = (TextView) findViewById(R.id.text_one);
-        mTestTextOne.setText("ColorScheme: " + mColorScheme);
+        //Main screen
+        FrameLayout mainView = (FrameLayout) findViewById(R.id.game_view);
 
-        mTestTextTwo = (TextView) findViewById(R.id.text_two);
-        mTestTextTwo.setText("IsGameHard: " + mIsGameHard);
+        // Create the game view and add it to the game screen.
+        mGameView = new jollyrogergaming.projectilemandate.GameView(this);
+        mainView.addView(mGameView);
+
+//        mTestTextOne = (TextView) findViewById(R.id.text_one);
+//        mTestTextOne.setText("ColorScheme: " + mColorScheme);
+//
+//        mTestTextTwo = (TextView) findViewById(R.id.text_two);
+//        mTestTextTwo.setText("IsGameHard: " + mIsGameHard);
     }
 
     @Override
--- app/src/main/java/jollyrogergaming/projectilemandate/GameView.java
+++ app/src/main/java/jollyrogergaming/projectilemandate/GameView.java
@@ -1,13 +1,24 @@
 package jollyrogergaming.projectilemandate;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by Kelton on 4/9/2016.
  */
 public class GameView extends View {
+
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private static final String TAG = "GameActivity";
+    private int mTouchX;
+    private int mTouchY;
 
     public GameView(Context context){
         this(context, null);
@@ -15,6 +26,49 @@ public class GameView extends View {
 
     public GameView(Context context, AttributeSet attrs){
         super(context, attrs);
+    }
+
+    // Android calls this to redraw the view, after invalidate()
+    @Override
+    protected void onDraw(Canvas canvas)    {
+        super.onDraw(canvas);
+        //Log.d(TAG, "onDraw(); X = " + mX + " Y = " + mY);
+
+        //Draw background, feel free to delete or change color. Here just in case we want a background color.
+        canvas.drawARGB(255, 88, 42, 114);
+
+        //Draw a circle at user touch
+        mPaint.setColor(0xFF00FF00);
+        canvas.drawCircle(mTouchX, mTouchY, 50, mPaint);
+
+        //Log.d(TAG, "onSensorChanged() " + mLightLevel);
+    }
+
+    // Sets the mTouchX and mTouchY whenever a user touches the screen.
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                mTouchX = (int)current.x;
+                mTouchY = (int)current.y;
+                Log.i(TAG, action + " at x =" + current.x + ", y =" + current.y);
+                Log.i(TAG, "x = " + mTouchX + ", y = " + mTouchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                break;
+        }
+        //Log.i(TAG, action + " at x =" + current.x + ", y =" + current.y);
+        return true;
     }
 
 
