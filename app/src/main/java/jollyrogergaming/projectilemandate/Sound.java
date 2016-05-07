6ee58d7efc69ab012e028479aa02a5f41ae0ebb7
Trevor Worth <tworth@mymail.mines.edu>
2016-05-06 23:01:17 -0600

    Fixed sounds

---
+++ app/src/main/java/jollyrogergaming/projectilemandate/Sound.java
@@ -0,0 +1,33 @@
+package jollyrogergaming.projectilemandate;
+
+/**
+ * Created by Trevor on 5/6/2016.
+ */
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+    private Integer mSoundId;
+
+    public Sound(String assetPath){
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".mp3", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
+    }
+}
